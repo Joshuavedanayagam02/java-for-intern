bf05c0e6088d5a6048e33cd5883b1dd7c1d39873
Joshuavedanayagam02 <99581781+Joshuavedanayagam02@users.noreply.github.com>
2022-02-13 18:59:00 +0530

    precom.java

---
+++ precom.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class precom
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter 1st value");
+    int a=sc.nextInt();
+    System.out.println("enter 2nd value");
+    int b=sc.nextInt();
+    int i,n=1,r=1,v=1;
+    float permutation,combination;
+    for(i=1;i<=a;i++)
+    {
+      n=n*i;
+    }
+    for(i=1;i<=b;i++)
+    {
+      r=r*i;
+    }
+    for(i=1;i<=a-b;i++)
+    {
+      v=v*i;
+    }
+    permutation=n/v;
+    System.out.println("permutation value is"+permutation);
+    combination=n/(r*v);
+    System.out.println("combination value is"+combination);
+  }
+}
